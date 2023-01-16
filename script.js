ed3c8a6dc676f7380aeb155c70cdd1e5f90740fd
Samantha Orozco <orozcoxs@gmail.com>
2023-01-15 17:43:06 -0800

    generated prompts

--- script.js
+++ script.js
@@ -5,13 +5,40 @@ var specialCharArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']'
 var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ];
 var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ];
+var charSets = [];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePrompt() {
+  if (confirm("Include special characters?")) {
+    charSets = charSets.concat(specialCharArr);
+  }
+  
+  if (confirm("Include lowercase characters?")) {
+    charSets = charSets.concat(lowerCaseArr);
+  }
+
+  if (confirm("Include uppercase characters?")) {
+    charSets = charSets.concat(upperCaseArr);
+  }
+
+  if (confirm("Include numbers?")) {
+    charSets = charSets.concat(numberArr);
+  }
+}
+
 function generatePassword() {
   console.log("");
+  var passwordLength = prompt("How many characters do you want the password to be? (8 - 128 characters")
   
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    generatePrompt(); 
+  }
+  else {
+    alert("Character length has to be a number between 8 and 28 digits.");
+  }
+
   // 1. Prompt the user for the password criteria.
   //      a. password length between 8 < 128.
   //      b. lowercase, uppercase, special characters.
