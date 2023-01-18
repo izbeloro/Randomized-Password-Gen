bce05fbb937db6094b4de6da2ab0cf43d2cfe823
Samantha Orozco <orozcoxs@gmail.com>
2023-01-17 21:48:52 -0800

    fixed bugged code

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
 var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ];
 var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ];
 var charSets = [];
-var password = "";
 
 var generateBtn = document.querySelector("#generate");
 
 function generatePrompt() {
+  charSets = [];
   if (confirm("Include special characters?")) {
     charSets = charSets.concat(specialCharArr);
   }
@@ -26,21 +26,23 @@ function generatePrompt() {
 }
 
 function randomizeChar(passwordLength) {
+  retVal = ""
   for (var i = 0; i < passwordLength; i++) {
-    password += charSets[Math.floor(Math.random() * charSets.length)]
+    retVal += charSets[Math.floor(Math.random() * charSets.length)]
   }
+  return retVal;
 }
 
 function generatePassword() {
   var passwordLength = window.prompt("How many characters do you want the password to be? (8 - 128 characters")
   if (passwordLength >= 8 && passwordLength <= 128) {
     generatePrompt(); 
-    var randomPassword = randomizeChar(passwordLength);
+    randomizeChar(passwordLength);
   }
   else {
     alert("Character length has to be a number between 8 and 28 digits.");
   }
-  return password;
+  return retVal;
 }
 
 // Write password to the #password input
